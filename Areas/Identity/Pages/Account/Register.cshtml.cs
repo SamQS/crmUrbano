36e2b5aba2af923b7b281c54464188d0a336ae62
Kevin <kevin.punk.141@gmail.com>
2022-06-02 00:56:03 -0500

    k

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,19 +46,19 @@ namespace crmUrbano.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
+            [EmailAddress(ErrorMessage = "Dirección de correo electrónico inválida.")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "Asegúrate de que la contraseña tenga al menos seis caracteres, incluido un número o carácter especial, y no sea una contraseña habitual. ", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "Las contraseñas no coinciden")]s
             public string ConfirmPassword { get; set; }
         }
 
